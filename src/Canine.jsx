f6fda7c0a65bf4029f00d1f6a1b7162c55ea6b13
soleilyasmina <mounlasolomon@gmail.com>
2020-10-04 18:12:46 -0400

    remove solution code

--- src/Canine.jsx
+++ src/Canine.jsx
@@ -1,33 +1,9 @@
 import React, { Component } from "react";
-import axios from "axios";
 
 class Canine extends Component {
-  constructor() {
-    super();
-    this.state = {
-      breeds: [],
-    };
-  }
-
-  async apiCall() {
-    const dogURL = "https://dog.ceo/api/breeds/list/all";
-    const response = await axios.get(dogURL);
-    const dogBreeds = Object.keys(response.data.message);
-    this.setState({ breeds: dogBreeds });
-  }
-
-  componentDidMount() {
-    this.apiCall();
-  }
-
   render() {
-    const { breeds } = this.state;
     return (
-      <ul>
-        {breeds.map((breed, index) => (
-          <li key={index}>{breed}</li>
-        ))}
-      </ul>
+      <h4>Render your dog breeds here!</h4> 
     );
   }
 }
